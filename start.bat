9f9e1b008f539d15c82ca7df7a089eec029f6fb7
Skek <skekung.yt@gmail.com>
2025-04-09 16:27:51 +0200

    Updated Readme

--- start.bat
+++
@@ -1,3 +0,0 @@
-start cmd /k python SpeechToText.py
-timeout /t 2 /nobreak > nul
-start cmd /k node main.js
--- start.sh
+++
@@ -1,3 +0,0 @@
-bash -c "python3 SpeechToText.py; exec bash"
-sleep 2
-bash -c "node main.js; exec bash"
